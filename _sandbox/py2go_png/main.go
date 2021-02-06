1ac3f907258b7a10f532b56f1356736da9e97047
ddddddO <lbfdeatq@gmail.com>
2021-02-07 01:12:29 +0900

    Add byte image to http response(go) in _sandbox

--- _sandbox/py2go_png/main.go
+++ _sandbox/py2go_png/main.go
@@ -1,10 +1,55 @@
 package main
 
 import (
+	"bytes"
+	"image"
+	"image/png"
 	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 )
 
+var pngByte []byte
+
 func main() {
-	io.Copy(os.Stdout, os.Stdin)
+	args := os.Args
+	if len(args) == 1 {
+		io.Copy(os.Stdout, os.Stdin)
+		return
+	}
+
+	if args[1] == "serve" {
+		log.Println("start")
+
+		var err error
+		pngByte, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		http.HandleFunc("/", pngHandler)
+
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func pngHandler(w http.ResponseWriter, r *http.Request) {
+	reader := bytes.NewReader(pngByte)
+	img, _, err := image.Decode(reader)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-type", "image/png")
+	err = png.Encode(w, img)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
 }
