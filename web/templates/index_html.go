d915d938508ece7c7b0c36e2039b371e5d23e669
ddddddO <lbfdeatq@gmail.com>
2021-02-14 03:05:01 +0900

    Modify web title

--- web/templates/index_html.go
+++ web/templates/index_html.go
@@ -7,7 +7,7 @@ const IndexHTML = `
 <head>
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Extxt</title>
+    <title>WordCloud</title>
 </head>
 <body>
     <form action="/" method="post" enctype="multipart/form-data">
